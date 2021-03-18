390ac4cd3d2c663c2dea5b3b8fa94c3d93ea93b0
agung3wi <agungskripsi05@gmail.com>
2021-03-18 10:40:17 +0700

    function dan unit test

--- app/helpers/function.php
+++ app/helpers/function.php
@@ -3,13 +3,15 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
-function hasPermission($task)
-{
-    $userId = Auth::id();
-    $permission = DB::selectOne("SELECT 1 FROM users A
+if (!function_exists('hasPermission')) {
+    function hasPermission($task)
+    {
+        $userId = Auth::id();
+        $permission = DB::selectOne("SELECT 1 FROM users A
             INNER JOIN role_task B ON B.role_id = A.role_id
             INNER JOIN tasks C ON B.task_id = C.id AND C.task_code = ?
         WHERE A.id = ?", [$task, $userId]);
-        
-    return !is_null($permission);
+
+        return !is_null($permission);
+    }
 }
